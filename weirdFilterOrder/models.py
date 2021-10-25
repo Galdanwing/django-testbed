17ec7ac970fef877b7bd82a0bf718a0d27d5d454
Antoine van der Horst <antoinevdhorst@gmail.com>
2021-10-25 21:48:40 +0200

    Update tests.

--- weirdFilterOrder/models.py
+++ weirdFilterOrder/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
-class CustomManager(models.Manager):
+class AlphabeticalOrderObjects(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(attribute_c="I should be first", attribute_b="I should be second", attribute_a = "I should be third")
+        # This order does not matter, it will become alphabetical
+        return super().get_queryset().filter(attribute_c=True, attribute_b=True, attribute_a=True)
+
+
+class FilteredOrderObjects(models.Manager):
+    def get_queryset(self):
+        # Because we filter separately here, order does matter
+        return super().get_queryset().filter(attribute_c=True).filter(attribute_b=True).filter(attribute_a=True)
+
 
 class ExampleModel(models.Model):
-    attribute_a = models.CharField(max_length=32)
-    attribute_b = models.CharField(max_length=32)
-    attribute_c = models.CharField(max_length=32)
+    attribute_a = models.BooleanField()
+    attribute_b = models.BooleanField()
+    attribute_c = models.BooleanField()
 
     class Meta:
         indexes = [
             models.Index(fields=['attribute_a', 'attribute_b', 'attribute_c']),
         ]
-    objects = CustomManager()
-    all_objects = models.Manager()
+
+    alphabetical_order_objects = AlphabeticalOrderObjects()
+    filter_order_objects = FilteredOrderObjects()
+    objects = models.Manager()
